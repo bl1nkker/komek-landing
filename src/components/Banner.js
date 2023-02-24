1a08a9b021a7e20072b0238c19c91c11d37f3c9e
bl1nkker <nurovich14@gmail.com>
2023-02-24 20:03:55 +0600

    feat: changed banner test

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,7 +4,8 @@ const Banner = () => {
       <div className="banner-text d-flex flex-column justify-content-center align-items-start">
         <p>We do it for people in need</p>
         <h1 className="text-start banner-title">
-          Make child's Life by giving of yours
+          Donation has never been easier, and so satisfying! Komek (in Kazakh
+          "help") - a passive donation app
         </h1>
         <a className="btn btn-outline-success mt-3" href="#form">
           Join Us
