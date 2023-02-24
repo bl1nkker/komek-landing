bc480269627853f9403cbaa6a5beefb664c582a5
bl1nkker <nurovich14@gmail.com>
2023-02-24 11:09:00 +0600

    feat: changed testimonials text

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -8,21 +8,21 @@ const testimonialData = [
     name: "Miras Dolayev",
     position: "CEO, MOST Academy",
     avatar: Testimonials1,
-    text: "This project has the potential to become popular and successful. Team have an interesting idea that combines elements of charity, loyalty and exclusivity, which can attract a large number of users.",
+    text: "I've been using Komek for a few months now, and I can honestly say that it's changed the way I think about giving back. I used to struggle with finding the time and motivation to research charities and make donations, but Komek takes care of everything for me. And it's not just about giving back – I feel like I'm also investing in my own well-being by treating myself to some of the self-love gifts. Thank you, Komek, for making a difference in the world and in my life.",
   },
   {
     id: 1,
     name: "Saltanat Berdikulova",
     position: "Senior Specialist, Kcell АО",
     avatar: Testimonials2,
-    text: "Komek App seems very interesting and innovative. The concept of a paid subscription with unique points for in-app purchases is original. Also, given the inclusion of charity in the project, your application can become not only entertaining, but also socially significant, attracting users who want to do good.",
+    text: "I never thought donating could be so easy and satisfying until I found Komek. It feels great to know that I'm making a difference in causes that matter to me, and I love that I can also invest in my own happiness with the self-love gifts. Plus, the app is so user-friendly that I hardly even notice the monthly donations coming out of my account. Highly recommend!",
   },
   {
     id: 1,
     name: "Diana Zharikova",
     position: "Head of Treasury, Al Hilal Bank Kazakhstan",
     avatar: Testimonials3,
-    text: "I, as a person who has devoted his whole life to charity, welcome your project with pleasure, which combines the possibilities of donation and loyalty in one application. I understand how important it is to have tools that can help people do good and help those in need, and your app is a great example of how technology can be used to achieve charitable causes",
+    text: "As someone who's always looking for ways to make a positive impact, Komek is a dream come true. I love that I can support the causes I care about without any extra effort on my part – and that I can also treat myself to some awesome products along the way. The app is so simple and straightforward, I barely even have to think about it. Thank you, Komek team, for creating such a wonderful platform!",
   },
 ];
 
