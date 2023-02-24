3adaa8d2a04b5722be868da7fc8693753ef34ceb
bl1nkker <nurovich14@gmail.com>
2023-02-24 20:06:37 +0600

    style: changed font size

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,8 +4,8 @@ const Banner = () => {
       <div className="banner-text d-flex flex-column justify-content-center align-items-start">
         <p>We do it for people in need</p>
         <h1 className="text-start banner-title">
-          Donation has never been easier, and so satisfying! Komek (in Kazakh
-          "help") - a passive donation app
+          Donation has never been easier, and so satisfying! Komek - a passive
+          donation app
         </h1>
         <a className="btn btn-outline-success mt-3" href="#form">
           Join Us
